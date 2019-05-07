034f303911d9400f097518beb4b5c5b06f29ad4e
Timothy Shores <timothymshores@gmail.com>
2019-05-07 11:31:21 -0500

    Import express session into server.js using require syntax

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session')
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
