cd2d2e47c9a816b99520c958072316d8a4995678
Timothy Shores <timothymshores@gmail.com>
2019-05-07 12:55:29 -0500

    Create login function

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -25,6 +25,7 @@ router.post('/login', (req, res) => {
         .first()
         .then(user => {
             if (user && bcrypt.compareSync(password, user.password)) {
+                req.session.username = user.username;
                 res.status(200).json({
                     message: `Welcome ${user.username}!`,
                 });
